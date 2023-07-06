5def4035803bc761b7b2276c8de2d7f88ff62a7c
Achraf <91100199+Achra-f@users.noreply.github.com>
2023-07-07 00:16:36 +0200

    form html / css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-import dropdowns from './Drop-down/dropDown';
-import carousel from './Carousel/carousel';
-
-const carousel1 = document.getElementById('carousel1');
-carousel(carousel1);
-
-const carousel2 = document.getElementById('carousel2');
-carousel(carousel2);
-
-const carousel3 = document.getElementById('carousel3');
-carousel(carousel3);
-
-dropdowns();
+import './Form/formValidation.css';
